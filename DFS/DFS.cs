9dbfab6802ff60d0e7200ec066b44640d813edd7
abhinavveenu <abhinavveenu@gmail.com>
2017-04-10 11:49:04 +0530

    DFS Algorithm
    Implemented DFS algorithm

---
+++ DFS/DFS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract
+{
+    interface DFS
+    {
+        void DFSTraverse(Node node);
+        void DFSTraverseRecursion(Node node);
+    }
+}
