f7c2767f9525cdb46ce3b845fead52ad89a5b36d
abhinavveenu <abhinavveenu@gmail.com>
2017-04-10 11:56:21 +0530

    Linked List operations and structure
    ### Linked list structure
    ### Operations
    # Detect Loop

---
+++ LinkedListOperations/LinkedList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract
+{
+    class LinkedList
+    {
+        public int Key { get; set; }
+        public LinkedList Next { get; set; }
+
+        public LinkedList(int key)
+        {
+            this.Key = key;
+        }
+
+        /// <summary>
+        /// Loop detect using slow and fast pointers
+        /// </summary>
+        /// <returns></returns>
+        public bool IsLooped()
+        {
+            LinkedList slowPointer = this;
+            LinkedList fastPointer = this;
+            while (slowPointer != null && fastPointer != null && fastPointer.Next!=null)
+            {
+                slowPointer = slowPointer.Next;
+                fastPointer = fastPointer.Next.Next;
+                if (slowPointer == fastPointer) return true;
+            } 
+                return false;
+        }
+    }
+}
